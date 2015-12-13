8cd951c96105114b9f6ca22a3012383dc21e3a25
Daniel Whiffing <daniel.whiffing@avidlifemedia.com>
2015-12-13 13:21:16 -0500

    Research is now serializable.  Need to optimize later phases of game

--- src/factories/ResearchFactory.js
+++ src/factories/ResearchFactory.js
@@ -1,40 +1,55 @@
-// TODO: move all research creation logic here
-research: {
+import Constants from 'utils/Constants'
+
+export default (id) => {
+  const buildingNames = Constants.building[id]
+  let researchTypes = research
+  if (id === 0) {
+    researchTypes = Object.assign({
+      extra: specialResearch.extraHamlets
+    }, researchTypes)
+  }
+  buildingNames.forEach((b, i) => {
+    researchTypes['autoBuy-'+i] = specialResearch.autoBuy
+  })
+  return researchTypes
+}
+
+const research = {
   discount: {
     rank: 0,
     current: 0,
     percent: true,
     increment: 0.05,
     max: 0.5,
-    description: opts => `${opts.name} buildings cost ${opts.value} less`
+    description: "{name} buildings cost {value} less"
   },
   ignoreCost: {
     rank: 0,
     current: 0,
     increment: 1,
     max: 10,
-    description: opts => `${opts.name} buildings ignore ${opts.value} buildings when computing cost`
+    description: "{name} buildings ignore {value} buildings when computing cost"
   },
   startMoney: {
     rank: 0,
     current: 0,
     increment: 200,
     max: 2000,
-    description: opts => `${opts.name}s start with ${opts.value} ${opts.currency}`
+    description: "{name}s start with {value} {currency}"
   },
   activeIncome: {
     rank: 0,
     current: 1,
     increment: 1,
     max: 5,
-    description: opts => `${opts.name}s generate ${opts.value} when clicking`
+    description: "{name}s generate {value} when clicking"
   },
   passiveIncome: {
     rank: 0,
     current: 0,
     increment: 5,
     max: 50,
-    description: opts => `${opts.name}s have ${opts.value} passive income`
+    description: "{name}s have {value} passive income"
   },
   upgradeRate: {
     rank: 0,
@@ -42,21 +57,21 @@ research: {
     percent: true,
     increment: 0.2,
     max: 2,
-    description: opts => `${opts.name}s generate upgrade points ${opts.value} faster`
+    description: "{name}s generate upgrade points {value} faster"
   },
   autoComplete: {
     rank: 0,
     current: 100,
     increment: -10,
     min: 0,
-    description: opts => `Auto Completes ${opts.name}s after ${opts.value} seconds`
+    description: "Auto Completes {name}s after {value} seconds"
   },
   incrementCost: {
     rank: 0,
     current: 4,
     increment: -1,
     min: 1,
-    description: opts => `Finish ${opts.value} ${opts.name}s before getting next`
+    description: "Finish {value} {name}s before getting next"
   },
   autoCost: {
     rank: 0,
@@ -64,16 +79,17 @@ research: {
     percent: true,
     increment: -0.05,
     min: 1,
-    description: opts => `Lower AutoBuy cost multiplier to ${opts.value}`
+    description: "Lower AutoBuy cost multiplier to {value}"
   }
-},
-specialResearch: {
+}
+
+const specialResearch = {
   extraHamlets: {
     rank: 0,
     current: 1,
     increment: 1,
     max: 5,
-    description: opts => `You can have ${opts.value} ${opts.name}s active at once`
+    description: "You can have {value} {name}s active at once"
   },
   autoBuy: {
     rank: 0,
@@ -81,7 +97,6 @@ specialResearch: {
     percent: true,
     increment: 0.05,
     max: 0.5,
-    description: opts => `AutoBuy ${opts.value} of ${opts.building} per tick`
+    description: "AutoBuy {value} of {building} per tick"
   }
-}
 }
