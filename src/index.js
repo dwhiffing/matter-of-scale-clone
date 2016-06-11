10d9ed9722c90b943e8a997fdae87e73b362ca9e
Daniel Whiffing <danielwhiffing@gmail.com>
2016-06-11 12:30:38 -0400

    Fixes react router

--- src/index.js
+++ src/index.js
@@ -9,14 +9,14 @@ import PropertyView from 'views/PropertyView'
 import InstanceView from 'views/InstanceView'
 import ResearchView from 'views/ResearchView'
 
-import { Router, Route, IndexRoute } from 'react-router'
+import { Router, Route, IndexRoute, hashHistory } from 'react-router'
 import { Provider } from 'react-redux'
 
 import 'index.css'
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router>
+    <Router history={hashHistory}>
       <Route path="/" component={ApplicationView}>
         <Route path="/property" component={PropertyView} />
         <Route path="/research/:property" component={ResearchView} />
--- src/views/PropertyView.jsx
+++ src/views/PropertyView.jsx
@@ -2,77 +2,85 @@ import React from 'react'
 import InstanceLineItem from 'views/components/InstanceLineItem'
 import { format as f, titleify, Color } from 'utils/helpers'
 
-export default ({ properties, history, markInstanceComplete }) => {
-  return (
-    <div>
-    {Object.values(properties).map((property, i) => {
-      if (!property.unlocked) return false
+export default class PropertyView extends React.Component {
+  static contextTypes = {
+    router: React.PropTypes.object.isRequired,
+  };
 
-      const instances = property.getInstances()
-      const name = titleify(property.name)
-      const income = f(property.income(), '0,0')
-      const money = f(property.money(), '0,0')
-      const research = f(property.researchMoney, '0')
+  render() {
+    const { properties, markInstanceComplete } = this.props
+    return (
+      <div>
+      {Object.values(properties).map((property, i) => {
+        if (!property.unlocked) return false
 
-      const currencyName = <Color>{titleify(property.currencyName)}</Color>
-      const researchName = <Color>{titleify(property.researchName)}</Color>
-      const researchLink = (
-        <a href={`#/research/${property.id}`}>{name} Improvements</a>
-      )
+        const instances = property.getInstances()
+        const name = titleify(property.name)
+        const income = f(property.income(), '0,0')
+        const money = f(property.money(), '0,0')
+        const research = f(property.researchMoney, '0')
 
-      let last, lastName, next
-      if (i > 0) {
-        last = properties[i - 1]
-        lastName = titleify(last.name)
-        next = last.toCompleteUntilNextInstance
-      }
+        const currencyName = <Color>{titleify(property.currencyName)}</Color>
+        const researchName = <Color>{titleify(property.researchName)}</Color>
+        const researchLink = (
+          <a href={`#/research/${property.id}`}>{name} Improvements</a>
+        )
 
-      return (
-        <div className={property.name} key={i}>
+        let last, lastName, next
+        if (i > 0) {
+          last = properties[i - 1]
+          lastName = titleify(last.name)
+          next = last.toCompleteUntilNextInstance
+        }
 
-          <h3>{name}s</h3>
+        return (
+          <div className={property.name} key={i}>
 
-          {i > 0 && next > 0 &&
-            <p>
-              {lastName}s til next {property.name}: {next}
-            </p>
-          }
+            <h3>{name}s</h3>
 
-          {instances &&
-            <div>
+            {i > 0 && next > 0 &&
               <p>
-                {money} {currencyName} stored and producing {income} per tick
+                {lastName}s til next {property.name}: {next}
               </p>
+            }
 
-              <p>
-                {research} {researchName} is available for use on {researchLink}.
-              </p>
+            {instances &&
+              <div>
+                <p>
+                  {money} {currencyName} stored and producing {income} per tick
+                </p>
 
-              <p>
-                {property.completed} {name}s have been completed total.
-              </p>
+                <p>
+                  {research} {researchName} is available for use on {researchLink}.
+                </p>
 
-              <h4>
-                Active {name}s <span className="badge">{instances.length}</span>
-              </h4>
+                <p>
+                  {property.completed} {name}s have been completed total.
+                </p>
 
-              <ul className="list-group">
-                {instances.map((instance, i) => {
-                  return (
-                    <InstanceLineItem
-                      key={i}
-                      instance={instance}
-                      clickInstance={id => history.push(`/instance/${id}`)}
-                      clickComplete={id => markInstanceComplete(id)}>
-                    </InstanceLineItem>
-                  )
-                })}
-              </ul>
-            </div>
-          }
-        </div>
-      )
-    })}
-    </div>
-  )
+                <h4>
+                  Active {name}s <span className="badge">{instances.length}</span>
+                </h4>
+
+                <ul className="list-group">
+                  {instances.map((instance, i) => {
+                    return (
+                      <InstanceLineItem
+                        key={i}
+                        instance={instance}
+                        clickInstance={id => this.context.router.push(`/instance/${id}`)}
+                        clickComplete={id => markInstanceComplete(id)}>
+                      </InstanceLineItem>
+                    )
+                  })}
+                </ul>
+              </div>
+            }
+          </div>
+        )
+      })}
+      </div>
+    )
+
+  }
 }
